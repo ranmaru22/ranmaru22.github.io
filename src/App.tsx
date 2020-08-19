95c2984c13c44ea768e7d86391449292f9081dd4
Alex Sun <alexsun82@icloud.com>
2020-08-19 19:13:03 +0200

    Add projects section

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from "react";
 
 import Header from "./components/header";
 import NavBar from "./components/navbar";
+import Projects from "./components/projects";
 import About from "./components/about";
+
+import { projects } from "./projectData";
 
 const App: React.FC = () => {
     return (
         <>
             <NavBar />
             <Header />
+            <Projects projects={projects} />
             <About />
         </>
     );
