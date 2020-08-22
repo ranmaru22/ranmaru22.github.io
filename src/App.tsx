f0d18381398f1deb5ff5d0fc82005a25597517a5
Alex Sun <alexsun82@icloud.com>
2020-08-22 22:16:32 +0200

    Add footer section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/header";
 import NavBar from "./components/navbar";
 import Projects from "./components/projects";
 import About from "./components/about";
+import Footer from "./components/footer";
 
 import { projects } from "./projectData";
 
@@ -14,6 +15,7 @@ const App: React.FC = () => {
             <Header />
             <Projects projects={projects} />
             <About />
+            <Footer />
         </>
     );
 };
