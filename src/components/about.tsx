ab4a80948fc0d558cec1aeadd9afb056e26881e6
Alex Sun <alexsun82@icloud.com>
2020-08-23 01:38:12 +0200

    Add email link

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import { faGithub, faStackOverflow, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const About: React.FC = () => {
     return (
-        <section id="about">
-            <Container className="p-5 text-justify" fluid="md">
+        <section id="about" className="pt-5 bg-light">
+            <Container className="mt-5 pt-5 text-justify" fluid="md">
                 <Row>
                     <Col>
-                        <h3>About me</h3>
+                        <h3 className="h-border">About me</h3>
                         <p>
                             I am a self-taught programmer and web developer currently living in
                             Germany. I've had a passion for computers since I was little and became
@@ -28,15 +29,19 @@ const About: React.FC = () => {
                         <p>
                             I speak English, German, Japanese, and a little bit of Mandarin Chinese.
                         </p>
+                        <h5 className="text-center mt-5">You can find me also on...</h5>
                         <p className="text-center">
+                            <a href="mailto:alexsun82@icloud.com">
+                                <Icon className="icon mr-3" icon={faEnvelope} size="lg" />
+                            </a>
                             <a href="https://github.com/ranmaru22">
-                                <Icon className="mr-3" icon={faGithub} size="lg" />
+                                <Icon className="icon mx-3" icon={faGithub} size="lg" />
                             </a>
                             <a href="https://stackoverflow.com/users/12570014/ranmaru">
-                                <Icon className="mx-3" icon={faStackOverflow} size="lg" />
+                                <Icon className="icon mx-3" icon={faStackOverflow} size="lg" />
                             </a>
                             <a href="https://www.linkedin.com/in/alexsun82/">
-                                <Icon className="mx-3" icon={faLinkedin} size="lg" />
+                                <Icon className="icon mx-3" icon={faLinkedin} size="lg" />
                             </a>
                         </p>
                     </Col>
